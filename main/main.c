d3c559dd8380f8829ad511fcf61e0e8256b8fe5b
donnyunn <donnyunn@gmail.com>
2021-08-16 21:40:49 +0900

    2021-08-16 2

--- main/main.c
+++ main/main.c
@@ -15,17 +15,6 @@
 
 #include "main.h"
 
-#define TAG "MAIN"
-
-int get_time(void)
-{
-    int result;
-    struct timeval rawtime;
-    gettimeofday(&rawtime, NULL);
-    result = rawtime.tv_sec * 1000 + rawtime.tv_usec/1000;
-    return result;
-}
-
 void app_main(void)
 {
     mpu6050_t imu;
@@ -49,21 +38,4 @@ void app_main(void)
     } while (gatts_isConnected());
 
     deepsleep_init(9);
-
-    // while (1) {
-    //     if (!gatts_isConnected()) {
-    //         vTaskDelay(10 / portTICK_PERIOD_MS);
-    //         mpu6050_read(&imu);
-    //         packet_push(imu.data);
-    //         vTaskDelay(1000 / portTICK_PERIOD_MS);
-    //         deepsleep_init(9);
-    //     } else {
-    //         while (!gatts_isConnected()) {
-    //             printf(".");
-    //             vTaskDelay(500 / portTICK_PERIOD_MS);
-    //         }
-    //     }
-        
-    //     vTaskDelay(10 / portTICK_PERIOD_MS);
-    // }
 }
